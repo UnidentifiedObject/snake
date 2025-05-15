023b937016edcf74ba7232e49a9830f6a1271374
Cevdet <104096159+UnidentifiedObject@users.noreply.github.com>
2025-05-15 19:57:35 +0300

    Add files via upload

---
+++ Positions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    public class Positions
+    {
+        public int Row { get; }
+        public int Col { get; }
+
+        public Positions(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public Positions Translate(Direction dir)
+        {
+            return new Positions(Row + dir.RowOffset, Col + dir.ColOffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Positions positions &&
+                   Row == positions.Row &&
+                   Col == positions.Col;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Col);
+        }
+
+        public static bool operator ==(Positions left, Positions right)
+        {
+            return EqualityComparer<Positions>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Positions left, Positions right)
+        {
+            return !(left == right);
+        }
+    }
+}
